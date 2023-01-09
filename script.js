2b2385e83da2a8c5a97636581c7a15a198c994de
Mario Lima <limamario441@gmail.com>
2023-01-09 14:07:34 -0300

    myportfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirMenu(){
+    let menuMobile = document.querySelector('.header-mobile')
+        if(menuMobile.classList.contains('abrir')) {
+            menuMobile.classList.remove('abrir');
+        } else {
+            menuMobile.classList.add('abrir');
+        }
+    }
+ 
